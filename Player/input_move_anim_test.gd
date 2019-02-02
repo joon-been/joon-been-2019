370062ca4b454e6122229a3515d24e83519c40b0
Leo <lyubean@gmail.com>
2019-02-02 13:40:07 -0800

    Model faces towards velocity
    Lerping rotation towards the velocity is something for another day

--- Player/input_move_anim_test.gd
+++ Player/input_move_anim_test.gd
@@ -1,7 +1,9 @@
 extends KinematicBody
 
-export var move_speed = 8 #pixels per second in 60 fps
-var move_vec = Vector3() #vector that is applied to the KinematicBody
+export(int,0,60) var move_speed = 8 #pixels per second in 60 fps
+export(float,0,1) var rotate_speed = 1 #how fast to lerp between rotation targets
+var move_vec = Vector3(0,0,0) #vector that is applied to the KinematicBody
+var look_vec = Vector3(0,0,0) #vector that stores where the player should face between frames
 
 func _ready():
 	print("Move using WASD")
@@ -22,6 +24,13 @@ func _process(delta):
 	else:
 		move_vec.x = 0
 	
-	move_and_collide(move_vec.normalized() * move_speed * delta)
+	#look_vec = rotation
+	#print("1:",look_vec)
+	#var lookat = Vector3 ( translation + move_vec )
+	look_vec = Vector3 ( lerp(look_vec.x,translation.x+(move_vec.x*move_speed),rotate_speed) , translation.y , lerp(look_vec.z,translation.z+(move_vec.z*move_speed),rotate_speed) )
+	look_at(look_vec,Vector3(0,1,0))
 	
-	pass
+	#print(rotation)
+	
+	move_and_collide(move_vec * move_speed * delta)
+	#print("2:",look_vec)
