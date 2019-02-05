a06b24d658417c2ce9a719f1790d298c35b6b74e
jsr <raydialow@gmail.com>
2019-02-05 11:38:08 -0800

    deleted:    Control.tscn 	new file:   MAIN.tscn 	new file:   Node.gd 	new file:   PC.gd 	modified:   PC.tscn 	new file:   Title.gd 	modified:   Title.tscn 	new file:   message.gd 	new file:   message.tscn 	new file:   next button.gd 	new file:   playbutton.gd 	modified:   project.godot

---
+++ Node.gd
@@ -0,0 +1,18 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var title_scene
+var map_scene
+
+func _ready():
+	title_scene = load("res://Title.tscn").instance()
+	add_child(title_scene)
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
